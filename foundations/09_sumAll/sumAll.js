ef88a5737cdff0d4e16ac68a1cc4e4036e83ec20
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-10 20:32:17 +0200

    Finish foundations 09 exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR"
+    if(a < 0 || b < 0)
+        return "ERROR"
+    
+    // ensure range [min; max]
+    let min = a, max = b;
+    if(a > b)
+        min = b, max = a;
 
+    let result = 0;
+    for (let value = min; value <= max; value++) {
+        result += value;
+    }
+    return result;
 };
 
 // Do not edit below this line
