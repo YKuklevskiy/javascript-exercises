3d601c5aaf7280e696dcce831c6a5f5e23208389
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-10 20:37:32 +0200

    Finish foundations 10 exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleBy4 = year % 4 === 0;
+    const divisibleBy100 = year % 100 === 0;
+    const divisibleBy400 = year % 400 === 0;
 
+    if (divisibleBy400)
+        return true;
+    return divisibleBy4 && !divisibleBy100;
 };
 
 // Do not edit below this line
