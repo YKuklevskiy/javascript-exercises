8afdb425d610976e7c2bfd55469046173bac77ad
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-10 20:19:25 +0200

    Finish foundations 08 exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments).slice(1);
+    return arr.filter(el => !args.find(value => el === value));
 };
 
 // Do not edit below this line
