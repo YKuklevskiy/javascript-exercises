b8e0b188e8d9128298d6adcaecf5403f03d2be5a
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-16 14:18:38 +0200

    Finish foundations 16 exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(element => {
+        if(!('yearOfDeath' in element))
+            element.yearOfDeath = new Date().getFullYear();
+    });
 
+    return people.reduce((oldestYet, person) => {
+        if (person.yearOfDeath - person.yearOfBirth > oldestYet.yearOfDeath - oldestYet.yearOfBirth)
+            return person;
+        return oldestYet;
+    });
 };
 
 // Do not edit below this line
