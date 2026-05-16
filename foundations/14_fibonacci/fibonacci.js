6e6fa059d736d5d623c6d242c29e0b7b683e16a7
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-16 13:57:16 +0200

    Finish foundations 14 exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n === NaN)
+        return "OOPS";
+    if (n < 0)
+        return "OOPS";
+    
+    if (n === 0)
+        return 0;
+    if(n <= 2)
+        return 1;
 
+    // yeah could do w/o the array
+    let arr = Array(n);
+    arr[0] = arr[1] = 1;
+    for (let i = 2; i < arr.length; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n-1];
 };
 
 // Do not edit below this line
