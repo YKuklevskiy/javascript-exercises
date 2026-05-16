520a1a59434de2f384d6d247850dea767b905b91
Ykuklevskiy <73666754+YKuklevskiy@users.noreply.github.com>
2026-05-16 13:45:10 +0200

    Complete foundations 13 exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    for(let i = 0; i < Math.ceil(str.length / 2); i++) {
+        const invIndex = str.length - 1 - i;
+        if(str[i] !== str[invIndex])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
